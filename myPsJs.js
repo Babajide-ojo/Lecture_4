9e269dcc62fe47b523ee0ca78dbdeea68d27919b
Babajide-ojo <ojobabajide629@gmail.com>
2021-03-12 02:30:48 +0100

    good

--- myPsJs.js
+++ myPsJs.js
@@ -15,3 +15,22 @@ window.onclick = function(event) {
         }
     }
 }
+
+
+
+function searchItem() {
+    let input = document.getElementById("search-bar").value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName("animals");
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = 'none';
+        } else {
+            x[i].style.display = "list-item";
+        }
+
+    }
+
+
+
+}
